60fcd2d5122197974c9f360d9f9f9287dfe2ef36
tctsigemura <sigemura@tokuyama.ac.jp>
2016-05-28 12:55:23 +0900

    トランスレータ版がまともに使えるように

--- lib/wrapper.h
+++ lib/wrapper.h
@@ -20,83 +20,31 @@
  */
 
 /*
- * wrapper.c : C-- 版と C 版で仕様が異なる関数など
+ * wrapper.h : wrapper.c 関数のプロトタイプ宣言
  *
- * 2016.05.26 : #include <wrapper.h> を追加
- * 2016.03.02 : mAlloc, fOpen を追加
- * 2016.02.26 : 初期バージョン
+ * 2016.05.26 : 初期バージョン
  *
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <wrapper.h>
 
-void *_addrAdd(void *a, int inc) {
-  return (char *)a + inc;
-}
-
-void *_aToA(void *a) {
-  return a;
-}
+void *_addrAdd(void *a, int inc);
+void *_aToA(void *a);
 
 // 以下は 64bit 環境では危険
 /*
-int _aToI(void *a) {
-  return (int)a;
-}
-
-void *_iToA(int i) {
-  return (void*)i;
-}
+int _aToI(void *a);
+void *_iToA(int i);
 */
 
 // TaC 版ではエラーチェックして終了する
-void * _mAlloc(int s) {
-  void *p = malloc(s);
-  if (p==NULL) {
-    perror("malloc");
-    exit(1);
-  }
-  return p;
-}
+void * _mAlloc(int s);
 
 /* TaC 版ではエラー原因により null を返すので、
  * 以下では再現しきれていない
-FILE *_fOpen(char *n, char *m) {
-  FILE *fp = fopen(n, m);
-  if (fp==NULL) {
-    perror("fopen");
-    exit(1);
-  }
-  return fp;
-}
+FILE *_fOpen(char *n, char *m);
 */
 
 // TaC 版では string.cmm に記述されている関数
-
-// 文字を探す
-int strChr(char *s, int c) {
-  char *p = strchr(s, c);
-  if (p==NULL) return -1;
-  return p - s;
-}
-
-// 最後の文字を探す
-int strRchr(char *s, int c) {
-  char *p = strrchr(s, c);
-  if (p==NULL) return -1;
-  return p - s;
-}
-
-// 文字列を探す
-int strStr(char *s1, char *s2) {
-  char *p = strstr(s1, s2);
-  if (p==NULL) return -1;
-  return p - s1;
-}
-
-// 部分文字列を返す
-char *subStr(char *s, int pos) {
-  return s + pos;
-}
+int strChr(char *s, int c);
+int strRchr(char *s, int c);
+int strStr(char *s1, char *s2);
+char *subStr(char *s, int pos);
