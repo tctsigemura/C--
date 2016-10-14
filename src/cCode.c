dfced56601fdebea6136c440f84e6e3c1abbccf3
tctsigemura <sigemura@tokuyama.ac.jp>
2016-10-14 15:56:28 +0900

    トランスレータでif文出力のバグ修正

--- src/cCode.c
+++ src/cCode.c
@@ -22,6 +22,7 @@
 /*
  * cCode.c : C--トランスレータのコード生成部
  *
+ * 2016.10.14         : 2016.10.09 の変更はNG、if文は"if(e){}else{}"と出力する
  * 2016.10.09         : if-else 文の本体が if 文の場合"{}"を出力する
  * 2016.09.19         : SyLABL を SyADDR に変更
  * 2016.09.18         : SyCHAR を SyCHR に変更
@@ -197,27 +198,17 @@ static void printExp(int node){
 static void printIf(int node){
   printf("if (");                                   // if(
   printExp(syGetLVal(node));                        // 条件式
-  printf(")");                                      // )
+  printf("){");                                     // ){
   traceTree(syGetRVal(node));                       // 文
+  printf("}\n");                                    // }
 }
 
 // if-else文を印刷する
 static void printEls(int node){
-  int nodeIf = syGetLVal(node);                     // if文
-  printf("if (");                                   // if(
-  printExp(syGetLVal(nodeIf));                      // 条件式
-  printf(")");                                     // )
-  int nodeThen = syGetRVal(nodeIf);                 // if-else文の本体が
-  if (nodeThen==SyNULL) {                           // 空文なら
-    printf(";");                                    //   ;
-  } else {                                          // 空文以外なら
-    int typeThen = syGetType(nodeThen);             // else無しif文だけ"{}"必要
-    if (typeThen==SyIF) printf("{\n");              // {
-    traceTree(nodeThen);                            // 文
-    if (typeThen==SyIF) printf("}");                // }
-  }
-  printf("else ");                                  // else
+  printIf(syGetLVal(node));
+  printf("else{");                                  // else{
   traceTree(syGetRVal(node));                       // 文
+  printf("}\n");                                    // }
 }
 
 // while文を印刷する
