a6f747659afeebb33a9078b16511686c1553c7d3
tctsigemura <sigemura@tokuyama.ac.jp>
2016-10-16 11:42:43 +0900

    テストデータにif文関連追加

---
+++ Test/c--/C.Opt/t700.c
@@ -0,0 +1,17 @@
+static int a;
+static void f(){
+if((a>1))if((a>7))(a=3);
+else (a=5);
+else (a=6);
+{if((a>2))if((a>8))(a=3);
+else (a=5);
+}if((a>3)){if((a>9))(a=3);
+}else (a=5);
+if((a>4))if((a>10))(a=3);
+else {if((a>11))(a=4);
+}else (a=5);
+if((a>5));
+else ;
+if((a>6));
+else ;
+}
