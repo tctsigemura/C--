3cca7111ba55651ef4641533d2678ee2d79ef522
h.sakagami <sakagami@MacBook-Air.local>
2017-01-08 23:56:09 +0900

    C--版完成

--- samples/divc--/StepByStep/deb.sh
+++ samples/divc--/StepByStep/deb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cc -E -DC -Wno-comment -std=c99 -nostdinc -DNWORD=16 -DNBYTE=8\
+cc -E -Wno-comment -std=c99 -nostdinc -DNWORD=16 -DNBYTE=8\
  -I/usr/local/cmmLib -I/usr/local/cmmInclude - < ${1} | ../../../src/c--
