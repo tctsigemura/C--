e3bdfd273bdfaa7a70394d8713dac2e437700828
渡邉　尚輝 <i14watanabenaoki@MacBook-Pro-2.local>
2019-02-19 09:28:59 +0900

    wrapper.cの完成版

--- lib/wrapper.c
+++ lib/wrapper.c
@@ -2,7 +2,7 @@
  * Programing Language C-- "Compiler"
  *    Tokuyama kousen Advanced educational Computer.
  *
- * Copyright (C) 2016 by
+ * Copyright (C) 2016 - 2018 by
  *                      Dept. of Computer Science and Electronic Engineering,
  *                      Tokuyama College of Technology, JAPAN
  *
@@ -22,6 +22,9 @@
 /*
  * wrapper.c : C-- 版と C 版で仕様が異なる関数など
  *
+ * 2018.11.17 : lToL を追加
+ * 2018.02.26 : fsize を追加
+ * 2018.02.20 : fseek を追加
  * 2016.08.07 : feof を追加
  * 2016.05.26 : #include <wrapper.h> を追加
  * 2016.03.02 : mAlloc, fOpen を追加
@@ -31,7 +34,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <wrapper.h>
+#include <sys/stat.h>
 
 void *_addrAdd(void *a, int inc) {
   return (char *)a + inc;
@@ -82,6 +85,20 @@ int _feof(FILE *fp) {
   return 0;
 }
 
+// TaC 版ではオフセットが上位と下位に分かれている
+int _fseek(FILE *stream, int offsh, int offsl){
+  return fseek(stream,((long)offsh<<32)|(long)offsl,SEEK_SET);
+}
+
+// ファイルのサイズを返す
+int fsize(char *path, int *size) {
+  struct stat sbuf;
+  if (lstat(path, &sbuf)<0) return 1;
+  size[0] = (sbuf.st_size >> 32) & 0xffffffff;
+  size[1] = sbuf.st_size & 0xffffffff;
+  return 0;
+}
+
 // TaC 版では string.cmm に記述されている関数
 
 // 文字を探す
@@ -108,4 +125,54 @@ int strStr(char *s1, char *s2) {
 // 部分文字列を返す
 char *subStr(char *s, int pos) {
   return s + pos;
+}
+
+
+int *_ICA(IA *x, int i, char *file, int line){
+  if(NULL==x){
+    fprintf(stderr,"nullPointer %s:%d行 添字%d \n",file,line,i);
+    abort();
+  }
+  if(i<0 || x->l<=i) {
+    fprintf(stderr,"%s:%d Out of Bound idx=%d\n",file,line,i);
+    abort();
+  }
+  return &(x->a[i]);
+}
+
+char *_CCA(CA *x, int i, char *file, int line){
+  if(NULL==x){
+    fprintf(stderr,"nullPointer %s:%d行 添字%d \n",file,line,i);
+    abort();
+  }
+  if(i<0 || x->l<=i) {
+    fprintf(stderr,"%s:%d Out of Bound idx=%d\n",file,line,i);
+    abort();
+  }
+  return &(x->a[i]);
+}
+
+void **_RCA(RA *x, int i, char *file, int line){
+  if(NULL==x){
+    fprintf(stderr,"nullPointer %s:%d行 添字%d \n",file,line,i);
+    abort();
+  }
+  if(i<0 || ((RA *)(x))->l<=i) {
+    fprintf(stderr,"%s:%d Out of Bound idx=%d\n",file,line,i);
+    abort();
+  }
+  return &(((RA *)(x))->a[i]);
+}
+
+void *_CP(void *p,char *file,int line){
+  if(p==NULL){
+    fprintf(stderr,"%d:%s Out of Bound \n",line,file);
+    abort();
+  }
+  return p;
+}
+
+// C-- の long を C の long にする
+long lToL(unsigned int l[]) {
+  return (((long)l[0])<<32)|l[1];
 }
