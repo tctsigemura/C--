aa7037ac113889e3d5021cf279b1ab4f2d1a24bf
tctsigemura <sigemura@tokuyama.ac.jp>
2019-02-27 22:27:54 +0900

    structをtypedefする改良の正解候補

--- Test/c--/C.Opt/t360.c
+++ Test/c--/C.Opt/t360.c
@@ -1,10 +1,12 @@
 void *__AddrAdd(void *_cmm_1P,int _cmm_2P);
 #include <stdio.h>
-struct Reloc {
+typedef struct _Reloc Reloc;
+struct _Reloc {
 int addr;
 int type_idx;
 };
-struct Symbol {
+typedef struct _Symbol Symbol;
+struct _Symbol {
 int type_sidx;
 int val;
 };
@@ -13,13 +15,13 @@ static int relocateSegment(int *_cmm_1P,int _cmm_2P,int *_cmm_3P,int *_cmm_4P){
 {
 int _cmm_1L;
 (_cmm_1L=0);
-for(;(_cmm_1L<_cmm_2P);(_cmm_1L=(_cmm_1L+sizeof(struct Reloc ))))
+for(;(_cmm_1L<_cmm_2P);(_cmm_1L=(_cmm_1L+sizeof(Reloc ))))
 {
-struct Reloc *_cmm_2L;
+Reloc *_cmm_2L;
 (_cmm_2L=__AddrAdd(_cmm_1P,_cmm_1L));
 int _cmm_3L;
-(_cmm_3L=((_cmm_2L->type_idx&16383)*sizeof(struct Symbol )));
-struct Symbol *_cmm_4L;
+(_cmm_3L=((_cmm_2L->type_idx&16383)*sizeof(Symbol )));
+Symbol *_cmm_4L;
 (_cmm_4L=__AddrAdd(_cmm_3P,_cmm_3L));
 (_cmm_4P[_cmm_2L->addr]=_cmm_4L->val);
 printf(_cmm_0S,_cmm_2L->addr,_cmm_4L->val,_cmm_3L,_cmm_4P[_cmm_2L->addr]);
