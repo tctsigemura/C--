4d7c142d211e8523d84ff0b7df3c24af53078796
tct-i15fujimura1s <fujimura1s.i15.tct@gmail.com>
2019-10-02 16:32:32 +0900

    conWriteに長さを指定するよう修正

--- lib/SrcTac/syslib.s
+++ lib/SrcTac/syslib.s
@@ -1,28 +1,28 @@
-; 
+;
 ; Programing Language C-- "Compiler"
 ;    Tokuyama kousen Advanced educational Computer
-; 
+;
 ;  Copyright (C) 2016 by
 ;                       Dept. of Computer Science and Electronic Engineering,
 ;                       Tokuyama College of Technology, JAPAN
-; 
+;
 ;    上記著作権者は，Free Software Foundation によって公開されている GNU 一般公
 ;  衆利用許諾契約書バージョン２に記述されている条件を満たす場合に限り，本ソース
 ;  コード(本ソースコードを改変したものを含む．以下同様)を使用・複製・改変・再配
 ;  布することを無償で許諾する．
-; 
+;
 ;    本ソースコードは＊全くの無保証＊で提供されるものである。上記著作権者および
 ;  関連機関・個人は本ソースコードに関して，その適用可能性も含めて，いかなる保証
 ;  も行わない．また，本ソースコードの利用により直接的または間接的に生じたいかな
 ;  る損害に関しても，その責任を負わない．
 ;
 
-; 
+;
 ; lib/syslib.s : システムコールに関するユーザ用ライブラリを集めたファイル
 ;
 ; 2016.02.27 : _exit を __exit に変更
 ; 2016.02.24 : TacOS の usr/lib からコピー
-; 
+;
 ; $Id$
 ;
 
@@ -48,6 +48,7 @@ _errno  dw      0           ; エラー番号
 ; 12     seek
 ; 13     conRead
 <<<<<<< HEAD
+<<<<<<< HEAD
 ; 14     conWrite 
 ; 15     malloc
 ; 16     free
@@ -59,6 +60,12 @@ _errno  dw      0           ; エラー番号
 ; 17     free
 
 >>>>>>> 6c7af53... Update syslib.s
+=======
+; 14     conWrite
+; 15     malloc
+; 16     free
+
+>>>>>>> 75f1191... conWriteに長さを指定するよう修正
 
 ; ユーザスタックに積まれたパラメータをカーネルスタックに積み直す
 ; ここでは、ユーザスタックから G1~G3 レジスタに取り出してレジスタ渡しを行う
@@ -71,14 +78,14 @@ _errno  dw      0           ; エラー番号
 
 ; ユーザスタックの状態(push g3後)
 ; addr  SP からの相対    スタックの状態
-; 00 
+; 00
 ;       SP+0            G3      今、SP はここを指している
 ;       SP+2            PC
 ;       SP+4            prm1
 ;       SP+6            prm2
 ;       SP+8            prm3
 ; FF
-        
+
 ; パラメータが1つの場合の処理
 .l1
         push    g3              ; G3 はレジスタ変数なので破壊してはいけない
@@ -113,7 +120,7 @@ _errno  dw      0           ; エラー番号
         st      g0,_errno       ; errno にシステムコールのエラー番号を格納
 .restr  pop     g3              ; G3 を復元
         ret
-        
+
 _exec
         ld      g0,#0           ; G0 にシステムコール番号を格納
         jmp     .l2
@@ -126,10 +133,10 @@ _wait
         ld      g0,#2           ; G0 にシステムコール番号を格納
         jmp     .l1
 
-_sleep  
-        ld      g0,#3           ; G0 にシステムコール番号を格納 
+_sleep
+        ld      g0,#3           ; G0 にシステムコール番号を格納
         jmp     .l1
-        
+
 _creat
         ld      g0,#4           ; G0 にシステムコール番号を格納
         jmp     .l1
@@ -165,7 +172,7 @@ _write
 _seek
         ld      g0,#12          ; G0 にシステムコール番号を格納
         jmp    .l3
-        
+
 _conRead
         ld      g0,#13          ; G0 にシステムコール番号を格納
         jmp     .l2
@@ -173,8 +180,13 @@ _conRead
 _conWrite
         ld      g0,#14          ; G0 にシステムコール番号を格納
 <<<<<<< HEAD
+<<<<<<< HEAD
         jmp     .l1
       
+=======
+        jmp     .l2
+
+>>>>>>> 75f1191... conWriteに長さを指定するよう修正
 ; malloc(#15) と free(#16) システムコールはユーザプロセスでは使用できない
 =======
         jmp     .l2
