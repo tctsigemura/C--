fbc2ac274e6d2034753687464446443012052805
tctsigemura <sigemura@tokuyama.kosen-ac.jp>
2021-03-20 15:04:56 +0900

    中間言語出力のテストを追加
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	new file:   IM.Opt/Makefile
    	new file:   IM.Opt/asm.sh
    	new file:   IM.Opt/t010.i
    	new file:   IM.Opt/t015.i
    	new file:   IM.Opt/t020.i
    	new file:   IM.Opt/t031.i
    	new file:   IM.Opt/t032.i
    	new file:   IM.Opt/t033.i
    	new file:   IM.Opt/t034.i
    	new file:   IM.Opt/t035.i
    	new file:   IM.Opt/t040.i
    	new file:   IM.Opt/t050.i
    	new file:   IM.Opt/t060.i
    	new file:   IM.Opt/t070.i
    	new file:   IM.Opt/t080.i
    	new file:   IM.Opt/t090.i
    	new file:   IM.Opt/t100.i
    	new file:   IM.Opt/t110.i
    	new file:   IM.Opt/t120.i
    	new file:   IM.Opt/t130.i
    	new file:   IM.Opt/t140.i
    	new file:   IM.Opt/t150.i
    	new file:   IM.Opt/t160.i
    	new file:   IM.Opt/t170.i
    	new file:   IM.Opt/t180.i
    	new file:   IM.Opt/t190.i
    	new file:   IM.Opt/t200.i
    	new file:   IM.Opt/t210.i
    	new file:   IM.Opt/t220.i
    	new file:   IM.Opt/t230.i
    	new file:   IM.Opt/t240.i
    	new file:   IM.Opt/t250.i
    	new file:   IM.Opt/t260.i
    	new file:   IM.Opt/t270.i
    	new file:   IM.Opt/t280.i
    	new file:   IM.Opt/t290.i
    	new file:   IM.Opt/t300.i
    	new file:   IM.Opt/t310.i
    	new file:   IM.Opt/t320.i
    	new file:   IM.Opt/t330.i
    	new file:   IM.Opt/t340.i
    	new file:   IM.Opt/t350.i
    	new file:   IM.Opt/t360.i
    	new file:   IM.Opt/t370.i
    	new file:   IM.Opt/t380.i
    	new file:   IM.Opt/t390.i
    	new file:   IM.Opt/t400.i
    	new file:   IM.Opt/t500.i
    	new file:   IM.Opt/t600.i
    	new file:   IM.Opt/t700.i
    	new file:   IM.Opt/t800.i
    	new file:   IM.Opt/test.sh
    	modified:   Makefile

---
+++ Test/c--/IM.Opt/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+INCDIR=/usr/local/cmmInclude
+LIBDIR=/usr/local/cmmLib
+
+if [ `uname` = Darwin ]; then
+  CPPFLAGS="-xc++ -Wno-comment -nostdinc -nostdlibinc -nobuiltininc" # macOS
+else
+  CPPFLAGS="-Wno-comment -nostdinc"                                 # Ubuntu
+fi
+
+for i in $*; do
+   j=`basename ${i}`
+   echo '[!!!' ${j} '!!!]'
+   n=`expr ${j} : '\([^\.]*\)'`
+   cpp ${CPPFLAGS} -I${INCDIR} -I${LIBDIR} ${i} |
+   ../../../src/ic-c-- -O > t.$$
+   diff ${n}.i t.$$
+done
+
+rm t.$$
